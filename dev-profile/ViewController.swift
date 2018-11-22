229acd17671f90dfe7c69e88f44e09f71d8e004f
Tijana Milicevic <tijana@MacBook-od-Korisnik.local>
2018-11-22 18:13:12 +0100

    first commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Tijana Milicevic on 11/22/18.
+//  Copyright © 2018 TT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
